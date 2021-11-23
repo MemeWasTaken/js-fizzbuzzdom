3f0a9ee24357d99c8fb366c13db5bb5cbf3259ff
MemeWasTaken <memelazzari02@gmail.com>
2021-11-23 14:58:40 +0100

    problem in words

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// SECTION MILESTONE 1 //
+    // 1) PrintInConsole numeri da 1 a 100.
+    // 2) Per multipli di 3 stampare "Fizz".
+    // 3) Per multipli di 5 stampare "Buzz".
+    // 4) Per multipli di 3 && 5  stampare FizzBuzz.
+
+// SECTION MILESTONE 2 //
+    // 5) Inserire in index.html un container.
+    // 6) "Appendere" elemento con numero/stringa nel DOM.
+
+// SECTION MILESTONE 3 //
+    // 7) Aggiungere nel ciclo le classi a seconda che siano o meno multipli di 3, 5 e 3 && 5.
+    // 8) Crare stili corrispondenti per le varie classi
