bbb8382c523d9208140ffc4b6a1be5135f9f88e4
MemeWasTaken <memelazzari02@gmail.com>
2021-11-23 15:22:02 +0100

    Section Milestone 1

--- js/script.js
+++ js/script.js
@@ -1,16 +1,31 @@
 // SECTION MILESTONE 1 //
+
     // 1) PrintInConsole numeri da 1 a 100.
     for (let i = 1; i <= 100; i++) {
-        console.log(i);
+        // console.log(i);
+
+        if (!((i % 3 == 0) || (i % 5 === 0))) {
+            // 5) Per tutti i numeri != dai divisori di 3 && 5 stampare se stessi.
+            console.log(i);
+
+        } else if ((i % 3 == 0) && (i % 5 === 0)) {
+            // 4) Per multipli di 5 && 3  stampare "FizzBuzz".
+            console.log(i, 'FrizzBuzz');
+            
+        } else if (i % 5 === 0) {
+            // 3) Per multipli di 5 stampare "Buzz".
+            console.log(i, 'Buzz');
+            
+        } else if (i % 3 === 0) {
+            // 2) Per multipli di 3 stampare "Fizz".
+            console.log(i, 'Frizz');
+        }
     }
-    // 2) Per multipli di 3 stampare "Fizz".
-    // 3) Per multipli di 5 stampare "Buzz".
-    // 4) Per multipli di 3 && 5  stampare FizzBuzz.
 
 // SECTION MILESTONE 2 //
-    // 5) Inserire in index.html un container.
-    // 6) "Appendere" elemento con numero/stringa nel DOM.
+    // 1) Inserire in index.html un container.
+    // 2) "Appendere" elemento con numero/stringa nel DOM.
 
 // SECTION MILESTONE 3 //
-    // 7) Aggiungere nel ciclo le classi a seconda che siano o meno multipli di 3, 5 e 3 && 5.
-    // 8) Crare stili corrispondenti per le varie classi
+    // 1) Aggiungere nel ciclo le classi a seconda che siano o meno multipli di 3, 5 e 3 && 5.
+    // 2) Crare stili corrispondenti per le varie classi
