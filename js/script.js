86dbe9a33ab23de6f58f12bfd401fbc0f047dd95
MemeWasTaken <memelazzari02@gmail.com>
2021-11-23 15:03:58 +0100

    for loop

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 // SECTION MILESTONE 1 //
     // 1) PrintInConsole numeri da 1 a 100.
+    for (let i = 1; i <= 100; i++) {
+        console.log(i);
+    }
     // 2) Per multipli di 3 stampare "Fizz".
     // 3) Per multipli di 5 stampare "Buzz".
     // 4) Per multipli di 3 && 5  stampare FizzBuzz.
