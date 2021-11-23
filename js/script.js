6ae18f21b1504172ac54baff9fe57b6772ff3451
MemeWasTaken <memelazzari02@gmail.com>
2021-11-23 15:38:08 +0100

    Stampare in Index risultato

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // SECTION MILESTONE 1 //
 
     // 1) PrintInConsole numeri da 1 a 100.
-    for (let i = 1; i <= 100; i++) {
+    /*for (let i = 1; i <= 100; i++) {
         // console.log(i);
 
         if (!((i % 3 == 0) || (i % 5 === 0))) {
@@ -10,7 +10,7 @@
 
         } else if ((i % 3 == 0) && (i % 5 === 0)) {
             // 4) Per multipli di 5 && 3  stampare "FizzBuzz".
-            console.log(i, 'FrizzBuzz');
+            console.log(i, 'FizzBuzz');
             
         } else if (i % 5 === 0) {
             // 3) Per multipli di 5 stampare "Buzz".
@@ -18,13 +18,37 @@
             
         } else if (i % 3 === 0) {
             // 2) Per multipli di 3 stampare "Fizz".
-            console.log(i, 'Frizz');
+            console.log(i, 'Fizz');
         }
-    }
+    } */
 
 // SECTION MILESTONE 2 //
     // 1) Inserire in index.html un container.
+    const ulJs = document.querySelector(".container")
     // 2) "Appendere" elemento con numero/stringa nel DOM.
+    for (let i = 1; i <= 100; i++) {
+        
+
+        if (!((i % 3 == 0) || (i % 5 === 0))) {
+            ulJs.innerHTML += '<li>' + i + '</li>';
+            console.log(i);
+
+        } else if ((i % 3 == 0) && (i % 5 === 0)) {
+            ulJs.innerHTML += '<li> FizzBuzz </li>';
+            console.log(i, 'FizzBuzz');
+            
+        } else if (i % 5 === 0) {
+            ulJs.innerHTML += '<li> Buzz </li>';
+            console.log(i, 'Buzz');
+            
+        } else if (i % 3 === 0) {
+            ulJs.innerHTML += '<li> Fizz </li>';
+            console.log(i, 'Fizz');
+        }
+    }
+
+
+
 
 // SECTION MILESTONE 3 //
     // 1) Aggiungere nel ciclo le classi a seconda che siano o meno multipli di 3, 5 e 3 && 5.
